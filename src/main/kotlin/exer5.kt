013b9a279990aac735853a878840f20f871a7a66
JoaoVitorPerin <joao.vperin@outlook.com>
2023-03-15 16:27:49 -0300

    resolução dos exercícios 5 e 6

---
+++ src/main/kotlin/exer5.kt
@@ -0,0 +1,21 @@
+//Crie uma classe "ContaBancaria" com os atributos "saldo" e "limite". Adicione um método chamado "saque"
+// que recebe um valor como parâmetro e subtrai do saldo da conta. Se o valor do saque for maior que o saldo da conta,
+// o método deve lançar uma exceção com a mensagem "Saldo insuficiente".
+
+fun main() {
+    class ContaBancaria(var saldo:Float, var limite:Float){
+        fun saque(valor:Float){
+            if (valor <= saldo) {
+                saldo -= valor
+                println("\nSaque: R$ $valor\nSaldo: R$ $saldo\n")
+            } else{
+                throw Exception("Saldo insuficiente!")
+            }
+        }
+    }
+
+    var conta1 = ContaBancaria(400f,1000f)
+
+    conta1.saque(100f)
+    conta1.saque(1000f)
+}
