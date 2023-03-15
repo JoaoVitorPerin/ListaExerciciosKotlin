0a6c080113f35954dd2081c5f393d460e31cdde8
JoaoVitorPerin <joao.vperin@outlook.com>
2023-03-15 20:40:02 -0300

    inicio resolução exer10

--- src/main/kotlin/exer10.kt
+++ src/main/kotlin/exer10.kt
@@ -1,2 +1,15 @@
 //Crie uma função que receba uma lista de strings e retorne uma lista com todas
 // as strings que começam com a letra "A", em ordem alfabética.
+
+fun organizaLista(lista:Array<String>){
+    lista.filter {  lista.any { it.startsWith("a") }}
+    lista.sort()
+    return println(lista)
+}
+
+var listaString:Array<String> = arrayOf("A dona aranha", "subiu pela parede veio",
+"a chuva forte e", "a derrubou")
+
+fun main() {
+    organizaLista(listaString)
+}
