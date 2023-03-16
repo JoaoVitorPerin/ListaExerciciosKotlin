bfe68e160d00f72e4a7af3f29be7901c2774f268
JoaoVitorPerin <joao.vperin@outlook.com>
2023-03-16 00:27:01 -0300

    Resolução do exer 9

--- src/main/kotlin/exer9.kt
+++ src/main/kotlin/exer9.kt
@@ -1,14 +1,18 @@
 //Crie uma classe "Triangulo" com os atributos "base" e "altura".
 // Adicione um método chamado "area" que calcula e retorna a área do triângulo.
 
-data class Triangulo(var base:Float, var altura:Float){
+data class Triangulo(var base:Int, var altura:Int){
     fun area(){
-        // var area = (this.base * this*altura) / 2
-        // println("Area do triangulo: $area")
+        var area = (this.base * this.altura) / 2
+        println("Area do triangulo: $area")
     }
 }
 
+private operator fun Int.times(triangulo: Triangulo) {
+
+}
+
 fun main() {
-    var tri = Triangulo(4f,2f)
+    var tri = Triangulo(8,12)
     tri.area()
 }
