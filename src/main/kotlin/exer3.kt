738389b8996dda5158c315dc327dd4eb11ab15f2
JoaoVitorPerin <joao.vperin@outlook.com>
2023-03-16 00:06:08 -0300

    Exer 3 reformulado

--- src/main/kotlin/exer3.kt
+++ src/main/kotlin/exer3.kt
@@ -3,10 +3,10 @@ import java.util.Objects
 //Crie uma classe chamada "Pessoa" com os atributos "nome" e "idade". Em seguida, crie uma lista de objetos "Pessoa"
 //e ordene a lista em ordem alfabética pelo atributo "nome".
 
-public class Pessoa(var nome:String, var idade:Int)
+class Pessoa(var nome:String, var idade:Int)
 
 fun main() {
-    var lista = arrayOf(Pessoa("Joao", 19), Pessoa("Igor", 19), Pessoa("Thiago", 19))
+    val lista = arrayOf(Pessoa("Joao", 19), Pessoa("Igor", 19), Pessoa("Thiago", 19))
     lista.sortBy { it.nome }
-    lista.forEach { println(it) }
+    lista.forEach { println(it.nome) }
 }
